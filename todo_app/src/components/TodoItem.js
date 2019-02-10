510e02e0efa90df9a3f85e080b69f32b6e797065
Bryan Rojas <bryroj14@gmail.com>
2019-02-10 01:46:53 -0800

    Learning React! woohoo

---
+++ todo_app/src/components/TodoItem.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            backgroundColor:'##e3e3e3',
+            padding: '1em',
+            borderBottom: '1px #cccccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }
+    }
+
+    render() {
+        const {id, title} = this.props.todo;
+        return (
+        <div style={this.getStyle()}>
+            <p>
+                <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/>
+                { ' '+ id + ') ' + title}
+            </p>
+        </div>
+        )
+    }
+}
+
+//PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+export default TodoItem
